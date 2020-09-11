13de5c7a8e850ad19f927f076add473bde5f24bf
Jennifer Jang <j.jang42@gmail.com>
2020-09-11 12:23:55 -0400

    Added docstrings, removed unused arguments

--- meanshiftpp.h
+++ meanshiftpp.h
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <unordered_map> 
+#include <map> 
 #include <vector>
 #include <string>
 #include <sstream>
@@ -11,6 +11,18 @@ using namespace std;
 void generate_offsets_cy(int d,
                          int base,
                          int * offsets) {
+    /*
+        Generate 3**d neighbors for any point.
+
+        Parameters
+        ----------
+        d: Dimensions
+        base: 3, corresponding to (-1, 0, 1)
+        offsets: (3**d, d) array of offsets to be added to 
+                 a bin to get neighbors
+
+    */
+
     int tmp_i;
 
     for (int i = 0; i < pow(base, d); i++) {
@@ -24,73 +36,79 @@ void generate_offsets_cy(int d,
 }
 
 
-string _to_str(int d, 
-               int * bin) {
-    ostringstream os;
-    for (int k = 0; k < d; k++) {
-        os << bin[k] << ",";
-    }
-    return string(os.str());
-}
-
-
 void shift_cy(int n,
               int d,
               int base,
               float bandwidth,
-              int * offsets, // 3**d by d
-              float * X_shifted) {  // n by d
+              int * offsets,
+              float * X_shifted) {
     /*
-        
+        Generate 3**d neighbors for any point.
+
+        Parameters
+        ----------
+        n: Number of samples
+        d: Dimension
+        base: 3, corresponding to (-1, 0, 1)
+        bandwith: Radius for binning points. Points are assigned to the 
+                  (d, ) bin corresponding to floor division by bandwidth
+        offsets: (3**d, d) array of offsets to be added to 
+                 a bin to get its neighbors
+        X_shifted: (n, d) array of new points after one iteration
+                   of shift
+
     */
 
-    string bin_str;
-    unordered_map< string, pair< vector<float>, int> > means;
+    map< vector<int>, pair< vector<float>, int> > means;
 
     int * bins = new int[n * d];
-    int * new_bin = new int[d];
+    vector<int> current_bin(d);
 
     for (int i = 0; i < n; i++) {
 
+        // Bin point
         for (int k = 0; k < d; k++) {
             bins[i * d + k] = X_shifted[i * d + k] / bandwidth;
+            current_bin[k] = bins[i * d + k];
         }
 
-        bin_str = _to_str(d, &bins[i * d]);
-
-        if (means.find(bin_str) == means.end()) {
-            means[bin_str] = make_pair(std::vector<float>(d, 0), 0);
+        if (means.find(current_bin) == means.end()) {
+            means[current_bin] = make_pair(std::vector<float>(d, 0), 0);
         }
     }
     
     for (int i = 0; i < n; i++) {
 
         for (int j = 0; j < pow(base, d); j++) {
+
+            // Get neighbor
             for (int k = 0; k < d; k++) {
-                 new_bin[k] = bins[i * d + k] + offsets[j * d + k];
+                 current_bin[k] = bins[i * d + k] + offsets[j * d + k];
             }
 
-            bin_str = _to_str(d, new_bin);
-
-            if (means.find(bin_str) != means.end()) {
+            // If neighbor exists, add it to the mean
+            if (means.find(current_bin) != means.end()) {
                 
                 for (int k = 0; k < d; k++) {
-                    means[bin_str].first[k] += X_shifted[i * d + k];
+                    means[current_bin].first[k] += X_shifted[i * d + k];
                 }
 
-                means[bin_str].second++;
+                means[current_bin].second++;
             }
         }
     }
 
+    // Set every point to the mean of its neighbors 
     for (int i = 0; i < n; i++) {
-        bin_str = _to_str(d, &bins[i * d]);
+
+        for (int k = 0; k < d; k++) {
+            current_bin[k] = bins[i * d + k];
+        }
         
         for (int k = 0; k < d; k++) {
-            X_shifted[i * d + k] = means[bin_str].first[k] * 1.0 / means[bin_str].second;
+            X_shifted[i * d + k] = means[current_bin].first[k] * 1.0 / means[current_bin].second;
         }
     }
     
     delete[] bins;
-    delete[] new_bin;
 }
--- meanshiftpp.pyx
+++ meanshiftpp.pyx
@@ -44,21 +44,36 @@ class MeanshiftPP:
     Parameters
     ----------
     
-    bandwidth: Number of neighbors required for a point to be labeled a core point. Works
-               in conjunction with eps_density
+    bandwidth: Radius for binning points. Points are assigned to the bin 
+               corresponding to floor division by bandwidth
+
+    threshold: Stop shifting if the L2 norm between iterations is less than
+               threshold
+
+    iterations: Maximum number of iterations to run
 
     """
 
-    def __init__(self, bandwidth, threshold=0.01):
+    def __init__(self, bandwidth, threshold=0.0001, iterations=None):
         self.bandwidth = bandwidth
         self.threshold = threshold
+        self.iterations = iterations
 
 
     def fit_predict(self, X):
         """
+        Determines the clusters in either `iterations` or when the L2 
+        norm of consecutive iterations is less than `threshold`, whichever 
+        comes first.
+        Each shift has two steps: First, points are binned based on floor 
+        division by bandwidth. Second, each bin is shifted to the 
+        weighted mean of its 3**d neighbors. 
+        Lastly, points that are in the same bin are clustered together.
+
         Parameters
         ----------
-        
+        X: Data matrix. Each row should represent a datapoint in 
+           Euclidean space
 
         Returns
         ----------
@@ -71,17 +86,17 @@ class MeanshiftPP:
 
         result = np.full(n, -1, dtype=np.int32)
 
-        cnt = 0
+        iteration = 0
         base =  3
         offsets = np.full((base**d, d), -1, dtype=np.int32)
         generate_offsets_np(d, base, offsets)
         
-        while True:
+        while iteration < iterations or not iterations:
           #print("Iteration: ", cnt, len(np.unique(X, axis=0)))
-          cnt += 1
+          iteration += 1
 
           shift_np(n, d, base, self.bandwidth, offsets, X_shifted)
-          
+
           if np.linalg.norm(np.subtract(X, X_shifted)) <= self.threshold:
             break
 
