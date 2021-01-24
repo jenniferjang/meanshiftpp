7bd3dd3537bab04ba18fd77818675c40fbafc495
jenniferjang <j.jang42@gmail.com>
2021-01-24 00:48:57 -0500

    Updated Meanshift++

--- meanshiftpp.pyx
+++ meanshiftpp.pyx
@@ -3,7 +3,7 @@ cimport numpy as np
 from sklearn.neighbors import KDTree
 from scipy.sparse import csr_matrix
 from datetime import datetime
-import pprint
+from collections import defaultdict, Counter
 
 
 cdef extern from "meanshiftpp.h":
@@ -17,6 +17,14 @@ cdef extern from "meanshiftpp.h":
                   float bandwidth,
                   int * offsets,
                   float * X_shifted)
+
+cdef extern from "utils.h":
+    void stitch_cy(int n,
+                      int m, 
+                      int threshold,
+                      int n_clusters,
+                      int * clusters,
+                      int * updated_clusters)
 
 cdef generate_offsets_np(d, 
                          base,
@@ -37,6 +45,19 @@ cdef shift_np(n,
              bandwidth,
              <int *> np.PyArray_DATA(offsets),
              <float *> np.PyArray_DATA(X_shifted))
+
+cdef stitch_np(n,
+               m,
+               threshold,
+               n_clusters,
+               np.ndarray[int, ndim=1, mode="c"] clusters,
+               np.ndarray[int, ndim=1, mode="c"] updated_clusters):
+    return stitch_cy(n,
+                     m,
+                     threshold,
+                     n_clusters,
+                     <int *> np.PyArray_DATA(clusters),
+                     <int *> np.PyArray_DATA(updated_clusters))
 
 
 class MeanShiftPP:
@@ -59,8 +80,68 @@ class MeanShiftPP:
         self.threshold = threshold
         self.iterations = iterations
 
+    def draw_box(self, X, x1, x2, y1, y2, cluster_vals=[0], mask_val=1):
+      X_box = X[y1:y2, x1:x2, :]
+      X_box = X_box.reshape((y2-y1) * (x2-x1), 3)
+      result = self.fit_predict(X_box)
+      result = result.reshape(y2-y1, x2-x1)
+      mask = np.full(X.shape[:2], -1, dtype=np.int32)
+      mask[y1:y2, x1:x2] = np.where(np.isin(result, cluster_vals), mask_val, -1)
+      obj = np.where(mask == mask_val)
+      center = np.mean(obj, axis=1, dtype=np.int32)
+      hist = Counter(tuple(map(tuple, np.floor(X[obj] / self.bandwidth))))
+      return result, mask, center, hist
 
-    def fit_predict(self, X):
+    def update_hist(self, X, mask, hist, mask_val=1): 
+      obj = np.where(mask == mask_val)
+      center = np.mean(obj, axis=1, dtype=np.int32)
+      hist = Counter(tuple(map(tuple, np.floor(X[obj] / self.bandwidth))))
+
+      return center, hist 
+
+    def track(self, X, center, mask, hist, length, width, mask_val=1, adjust_threshold=0.75):
+      l, w, _ = X.shape
+      iteration = 0
+
+      while not self.iterations or iteration < self.iterations:
+        iteration += 1
+
+        mask = np.full((l, w), -1, dtype=np.int32)
+        min_y = l
+        max_y = 0
+        min_x = w 
+        max_x = 0
+
+        # track_np(l, w, length, width, self.bandwidth, X, hist, mask)
+        
+        for i in range(length + 1):
+          for j in range(width + 1):
+            y = center[0] + i - int(length/2)
+            x = center[1] + j - int(width/2)
+            if x < 0 or x >= w or y < 0 or y >= l:
+              continue
+            bin_ = tuple(np.floor(X[y][x] / self.bandwidth))
+            if hist[bin_] > 0:
+              mask[y][x] = mask_val
+              min_y = min(min_y, y)
+              max_y = max(max_y, y)
+              min_x = min(min_x, x)
+              max_x = max(max_x, x)
+
+        new_center, hist = self.update_hist(X, mask, hist, mask_val=mask_val)
+        if max_y - min_y < adjust_threshold * length:
+          length = int(max_y - min_y)
+        if max_x - min_x < adjust_threshold * width:
+          width = int(max_x - min_x)
+
+        if np.linalg.norm(np.subtract(center, new_center)) <= self.threshold:
+            break
+
+        center = new_center
+
+      return center, mask, hist, length, width
+
+    def fit_predict(self, X, return_modes=False):
         """
         Determines the clusters in either `iterations` or when the L2 
         norm of consecutive iterations is less than `threshold`, whichever 
@@ -101,7 +182,10 @@ class MeanShiftPP:
             break
 
           X = np.copy(X_shifted)
-          
-        _, result = np.unique(X_shifted, return_inverse=True, axis=0)
+
+        modes, result = np.unique(X_shifted, return_inverse=True, axis=0)
         
+        if return_modes:
+          return modes, result
+
         return result
